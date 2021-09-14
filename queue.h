5c77f5a6f080f43cc589eb1f73336e3e47e30f72
HarlopFlores <71047903+HarlopFlores@users.noreply.github.com>
2021-09-14 11:01:00 -0500

    Update Queue

--- queue.h
+++ queue.h
@@ -3,4 +3,23 @@
 template <typename T>
 class QueueArray : public CircularArray<T> { 
     
+     public:
+        QueueArray(){
+            QueueArray(0);
+        }
+        QueueArray(int capacity){
+            this->array = new T[capacity];
+            this->capacity = capacity;
+            this->size = 0;
+        }
+        void enqueue(T data){
+
+            push_back(data);
+
+        }
+        void dequeue(){
+
+            pop_front();
+        }
+    
 };
