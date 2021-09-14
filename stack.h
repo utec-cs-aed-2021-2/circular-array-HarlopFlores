0e67981ca66e155dc6c4766c388278b12a67b1db
HarlopFlores <71047903+HarlopFlores@users.noreply.github.com>
2021-09-14 11:08:24 -0500

    Update Stack.h

--- stack.h
+++ stack.h
@@ -3,4 +3,25 @@
 template <typename T>
 class StackArray : public CircularArray<T> { 
     
+    StackArray(){
+        StackArray(0);
+    }
+    StackArray(int capacity){
+        this->array = new T[capacity];
+        this->capacity = capacity;
+
+        this->size = 0;
+    }
+
+    void Stackpush(T data){
+
+        push_back(data);
+    }
+
+    void Stackpop(){
+
+        pop_back();
+    }
+    
+    
 };
